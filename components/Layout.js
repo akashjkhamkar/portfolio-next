ee419145a6b5df3e2206d149c655e2f243bce2c3
akash <akash.khamkar40@gmail.com>
2021-10-26 17:33:14 +0530

    updated links

--- components/Layout.js
+++ components/Layout.js
@@ -18,19 +18,19 @@ const Layout = ({children}) => {
           <meta name="description" content="Hi I am Akash, I am Software Developer. This is my portfolio website. Here you can find updates related to me, my projects and blogs. "/>
 
           {/* <!-- Facebook Meta Tags --> */}
-          <meta property="og:url" content="https://www.akashkhamkar.in/"/>
+          <meta property="og:url" content="https://akashkhamkar.in/"/>
           <meta property="og:type" content="website"/>
           <meta property="og:title" content="Akash Khamkar"/>
           <meta property="og:description" content="Hi I am Akash, I am Software Developer. This is my portfolio website. Here you can find updates related to me, my projects and blogs. "/>
-          <meta property="og:image" content="https://www.akashkhamkar.in/siteimg.png"/>
+          <meta property="og:image" content="https://akashkhamkar.in/siteimg.png"/>
 
           {/* <!-- Twitter Meta Tags --> */}
           <meta name="twitter:card" content="summary_large_image"/>
           <meta property="twitter:domain" content="akashkhamkar.in"/>
-          <meta property="twitter:url" content="https://www.akashkhamkar.in/"/>
+          <meta property="twitter:url" content="https://akashkhamkar.in/"/>
           <meta name="twitter:title" content="Akash Khamkar"/>
           <meta name="twitter:description" content="Hi I am Akash, I am Software Developer. This is my portfolio website. Here you can find updates related to me, my projects and blogs. "/>
-          <meta name="twitter:image" content="https://www.akashkhamkar.in/siteimg.png"/>
+          <meta name="twitter:image" content="https://akashkhamkar.in/siteimg.png"/>
 
           {/* <!-- Meta Tags Generated via https://www.opengraph.xyz --> */}
                 

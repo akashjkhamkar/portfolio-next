8f2e4152f81bdb3aaa6af10f930a2f62edef119f
akash <akash.khamkar40@gmail.com>
2021-10-27 19:33:18 +0530

    aria-lable

--- components/Socials.js
+++ components/Socials.js
@@ -3,29 +3,29 @@ import Image from "next/image"
 const Socials = (props) => {
     return (
     <div className={`socials ${props.className}`}>
-        <a href="https://github.com/akashjkhamkar/" target="_blank" rel="noreferrer">
-        <Image 
-        width="20px"
-        height="20px"
-        src="/images/github.svg" alt="github" className="socialIcon"/>
+        <a aria-label="github" href="https://github.com/akashjkhamkar/" target="_blank" rel="noreferrer">
+            <Image 
+            width="20px"
+            height="20px"
+            src="/images/github.svg" alt="github" className="socialIcon"/>
         </a>
-        <a href="https://www.linkedin.com/in/akashjkhamkar/" target="_blank" rel="noreferrer">
-        <Image 
-        width="20px"
-        height="20px" 
-        src="/images/linkedin.svg" alt="github" className="socialIcon"/>
+        <a aria-label="portfolio" href="https://www.linkedin.com/in/akashjkhamkar/" target="_blank" rel="noreferrer">
+            <Image 
+            width="20px"
+            height="20px" 
+            src="/images/linkedin.svg" alt="github" className="socialIcon"/>
         </a>
-        <a href="mailto:akash.khamkar40@gmail.com" target="_blank" rel="noreferrer">
-        <Image 
-        width="20px"
-        height="20px"
-        src="/images/mail.svg" alt="github" className="socialIcon"/>
+        <a aria-label="email" href="mailto:akash.khamkar40@gmail.com" target="_blank" rel="noreferrer">
+            <Image 
+            width="20px"
+            height="20px"
+            src="/images/mail.svg" alt="github" className="socialIcon"/>
         </a>
-        <a href="/AkashKhamkar.pdf">
-        <Image 
-        width="20px"
-        height="20px"
-        src="/images/resume.svg" alt="github" className="socialIcon"/>
+        <a aria-label="resume" href="/AkashKhamkar.pdf">
+            <Image 
+            width="20px"
+            height="20px"
+            src="/images/resume.svg" alt="github" className="socialIcon"/>
         </a>
     </div>
     )
